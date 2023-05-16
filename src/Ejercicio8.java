c619c7e059be1ffb18adf3c7f7f586404a7a24cd
Iñigo Imaña <20203795+inigoim@users.noreply.github.com>
2023-05-16 20:57:32 +0200

    Funciona el Ejercicio8

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,60 +1,73 @@
 import com.mysql.cj.jdbc.MysqlDataSource;
 import oracle.jdbc.datasource.impl.OracleDataSource;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Ejercicio8 {
+    private static MysqlDataSource mds;
+    private static OracleDataSource ods;
+
     public static void main(String[] args) throws SQLException {
-        MysqlDataSource mds = new MysqlDataSource();
+        mds = new MysqlDataSource();
         mds.setURL("jdbc:mysql://dif-mysql.ehu.es:3306/DBC15?&useSSL=false");
         mds.setUser("DBC15"); mds.setPassword("DBC15");
 
-        OracleDataSource ods = new OracleDataSource();
+        ods = new OracleDataSource();
         ods.setURL("jdbc:oracle:thin:@vsids11.si.ehu.es:1521:gipuzkoa");
         ods.setUser("BDC15"); ods.setPassword("BDC15");
 
-        try (Connection conn = mds.getConnection()) {
-            anadirProcedimiento(conn);
-            llamarProcedimiento(conn);
+        String procedimiento = "UPDATE hotel SET Capacidad = Capacidad + 10 WHERE IdHotel = 'h94';";
+        anadirProcedimientoMySQL(procedimiento, "aumentarCapacidad");
+        anadirProcedimientoOracle(procedimiento, "aumentarCapacidad");
+
+        try (Connection conn = mds.getConnection();
+             CallableStatement cstmt = conn.prepareCall("{call aumentarCapacidad}")) {
+            cstmt.executeUpdate();
         } catch (SQLException e) {
-            System.err.println("Error al conectar");
+            System.err.println("Error al llamar al procedimiento en MySQL");
             System.err.print(e.getMessage());
         }
 
-        try (Connection conn = ods.getConnection()) {
-            anadirProcedimiento(conn);
-            llamarProcedimiento(conn);
+        try (Connection conn = ods.getConnection();
+             CallableStatement cstmt = conn.prepareCall("{call aumentarCapacidad}")) {
+            cstmt.executeUpdate();
         } catch (SQLException e) {
-            System.err.println("Error al conectar");
+            System.err.println("Error al llamar al procedimiento en Oracle");
             System.err.print(e.getMessage());
         }
     }
 
-    public static void anadirProcedimiento(Connection conn) {
-        String procedimiento =
-                "BEGIN " +
-                "UPDATE hotel SET Capacidad = Capacidad + 10 WHERE IdHotel = 'h94';" +
-                "END";
-        String anadirProcedimiento = "CREATE OR REPLACE PROCEDURE procedimientoX AS " + procedimiento;
+    public static void anadirProcedimientoMySQL(String procedimiento, String nombre) {
 
-        try (Statement stmt = conn.createStatement()) {
+        String borrarProcedimiento = String.format("DROP PROCEDURE IF EXISTS %s", nombre);
+        String anadirProcedimiento = String.format("CREATE PROCEDURE %s() %s ", nombre, procedimiento);
+
+        try (Connection conn = mds.getConnection();
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(borrarProcedimiento);
             stmt.executeUpdate(anadirProcedimiento);
         } catch (SQLException e) {
-            System.err.println("Error al añadir procedimiento");
-            System.err.print(e.getMessage());
+            System.err.println("Error al añadir procedimiento en MYSQL");
+            System.err.println(e.getMessage());
         }
     }
 
-    public static void llamarProcedimiento(Connection conn) {
-        String llamada = "CALL procedimientoX";
+    public static void anadirProcedimientoOracle(String procedimiento, String nombre) {
 
-        try (Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate(llamada);
+        String anadirProcedimiento = String.format("CREATE OR REPLACE PROCEDURE %s AS " +
+                "BEGIN %s END;", nombre, procedimiento);
+
+        System.out.println(anadirProcedimiento);
+
+        try (Connection conn = ods.getConnection();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(anadirProcedimiento);
         } catch (SQLException e) {
-            System.err.println("Error al llamar procedimiento");
-            System.err.print(e.getMessage());
+            System.err.println("Error al añadir procedimiento en Oracle");
+            System.err.println(e.getMessage());
         }
     }
 }
